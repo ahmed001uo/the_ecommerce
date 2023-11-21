7ce24e0b38be281e671bb1230be292fdd4914398
OEM Configuration (temporary user) <ahmeduo221@gmail.com>
2023-11-21 22:00:25 +0200

    fith

---
+++ lib/utilities/router.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:the_ecommerce/utilities/routes.dart';
+
+
+Route<dynamic> onGenerate(RouteSettings settings){
+  switch(settings.name){
+    case AppRoutes.landingPageRoute:
+    default:
+    return CupertinoPageRoute(builder: (_)=> ,);
+  }
+}
